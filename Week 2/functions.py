6b3dca83e2b907e90e6128ec320ae7a2d40cc992
prajwal shrestha <prajwalstha632@gmail.com>
2022-12-12 20:04:59 +0545

    added functions

---
+++ Week 2/functions.py
@@ -0,0 +1,26 @@
+# a function is a block of code that performs a specific task.
+
+# def add_one(num):
+#     result = num +1
+#     print(result)
+
+# add_one(6)
+
+# def greeting(name):
+#     print("Hello " + name)
+
+# greeting("Prajwal")
+
+# def cube(number):
+#     result = number * number * number
+#     return result
+
+# x = cube(2)
+# y = cube(3)
+
+# print(x + y)
+
+def greet(name: str) -> str:
+    print(name) 
+
+greet("Prajwal")
